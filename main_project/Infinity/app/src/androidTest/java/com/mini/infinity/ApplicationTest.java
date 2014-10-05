9f654c013623ccd81ea57a26292de6036fac6f8d
j.ciecierski <jakub.ciecier@gmail.com>
2014-10-05 10:19:30 +0200

    New project

--- main_project/Infinity/app/src/androidTest/java/com/mini/infinity/ApplicationTest.java
+++ main_project/Infinity/app/src/androidTest/java/com/mini/infinity/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.mini.infinity;
+package com.mini.infinity;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- main_project/Infinity/app/src/main/java/com/mini/infinity/MainActivity.java
+++ main_project/Infinity/app/src/main/java/com/mini/infinity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.mini.infinity;
+package com.mini.infinity;
 
 import android.app.Activity;
 import android.os.Bundle;
